381d90e89d0fa550c0ff7dd6b96382f3e0358a7a
Wyatt Greenway <th317erd@gmail.com>
2017-07-04 13:45:57 -0700

    Fixed function name scoping

--- index.js
+++ index.js
@@ -9,44 +9,48 @@ const { NativeModules } = require('react-native');
 const { DynamicFonts } = NativeModules;
 const loadedFonts = {};
 
+function loadFont(name, data, type, forceLoad) {
+  /* Check if this font was already loaded */
+  if (!forceLoad && loadedFonts[name])
+    return Promise.resolve(loadedFonts[name]);
+
+  if (!name)
+    throw new Error('Name is a required argument');
+
+  if (!data)
+    throw new Error('Data is a required argument');
+
+  /* Load font via native binary code */
+  return new Promise(function(resolve, reject) {
+    DynamicFonts.loadFont({
+      name: name,
+      data: data,
+      type: type
+    }, function(err, givenName) {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      /* Loaded successfully... resolve promise with "real" font name */
+      loadedFonts[name] = givenName;
+      resolve(givenName);
+    });
+  });
+}
+
+function loadFonts(_fontList, forceLoad) {
+  var fontList = _fontList;
+  if (!fontList)
+    return Promise.resolve([]);
+
+  if (!(fontList instanceof Array))
+    fontList = [fontList];
+
+  return Promise.all(fontList.filter((font) => font).map((font) => loadFont(font.name, font.data, font.type, forceLoad)));
+}
+
 module.exports = {
-	loadFont: function(name, data, type, forceLoad) {
-    /* Check if this font was already loaded */
-    if (!forceLoad && loadedFonts[name])
-      return Promise.resolve(loadedFonts[name]);
-
-		if (!name)
-			throw new Error('Name is a required argument');
-
-		if (!data)
-			throw new Error('Data is a required argument');
-
-    /* Load font via native binary code */
-		return new Promise(function(resolve, reject) {
-			DynamicFonts.loadFont({
-				name: name,
-				data: data,
-				type: type
-			}, function(err, givenName) {
-				if (err) {
-					reject(err);
-					return;
-				}
-
-        /* Loaded successfully... resolve promise with "real" font name */
-        loadedFonts[name] = givenName;
-				resolve(givenName);
-			});
-		});
-	},
-  loadFonts: function(_fontList, forceLoad) {
-    var fontList = _fontList;
-    if (!fontList)
-      return Promise.resolve([]);
-
-    if (!(fontList instanceof Array))
-      fontList = [fontList];
-
-		return Promise.all(fontList.filter((font) => font).map((font) => loadFont(font.name, font.data, font.type, forceLoad)));
-	}
+	loadFont: loadFont,
+  loadFonts: loadFonts
 }
